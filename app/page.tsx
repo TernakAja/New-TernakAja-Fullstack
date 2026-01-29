a27851416fbae277eb1af07095fd21cf40229354
Farrel Tobias Saputro <76392444+F4E12@users.noreply.github.com>
2026-01-29 10:04:08 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,58 +1,113 @@
-import { DeployButton } from "@/components/deploy-button";
-import { EnvVarWarning } from "@/components/env-var-warning";
-import { AuthButton } from "@/components/auth-button";
-import { Hero } from "@/components/hero";
-import { ThemeSwitcher } from "@/components/theme-switcher";
-import { ConnectSupabaseSteps } from "@/components/tutorial/connect-supabase-steps";
-import { SignUpUserSteps } from "@/components/tutorial/sign-up-user-steps";
-import { hasEnvVars } from "@/lib/utils";
-import Link from "next/link";
-import { Suspense } from "react";
+import x21 from "./2-1.png";
+import x41 from "./4-1.png";
+import x51 from "./5-1.png";
+import { Footer } from "./Footer";
+import { Frame } from "./Frame";
+import { FrameWrapper } from "./FrameWrapper";
+import { Navbar } from "./Navbar";
+import { Paragraph } from "./Paragraph";
+import { SmartLivestock } from "./SmartLivestock";
+import { WithAiAndIot } from "./WithAiAndIot";
+import cow1 from "./cow-1.png";
+import dashboardPympro1 from "./dashboard-pympro-1.png";
+import livestockDetailsFs2Dhx1 from "./livestock-details-fs2dhx-1.png";
+import pexelsPixabay4589911 from "./pexels-pixabay-458991-1.png";
 
-export default function Home() {
+export const HomePage = () => {
   return (
-    <main className="min-h-screen flex flex-col items-center">
-      <div className="flex-1 w-full flex flex-col gap-20 items-center">
-        <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
-          <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
-            <div className="flex gap-5 items-center font-semibold">
-              <Link href={"/"}>Next.js Supabase Starter</Link>
-              <div className="flex items-center gap-2">
-                <DeployButton />
-              </div>
-            </div>
-            {!hasEnvVars ? (
-              <EnvVarWarning />
-            ) : (
-              <Suspense>
-                <AuthButton />
-              </Suspense>
-            )}
+    <div className="bg-background w-full min-w-[1440px] min-h-[1024px] relative">
+      <Footer />
+      <div className="flex flex-col w-[1440px] h-[1024px] items-center absolute top-0 left-0">
+        <div className="relative w-[1440px] h-[1024px]">
+          <img
+            className="absolute top-0 left-0 w-[1440px] h-[1021px] aspect-[1.41]"
+            alt="Pexels pixabay"
+            src={pexelsPixabay4589911}
+          />
+
+          <div className="bg-primary-95 absolute top-0 left-0 w-[1440px] h-[1024px]" />
+
+          <img
+            className="absolute top-[325px] left-[395px] w-[699px] h-[699px] aspect-[1]"
+            alt="Cow"
+            src={cow1}
+          />
+
+          <div className="flex flex-col items-center justify-between px-0 py-[124px] absolute top-0 left-0 w-[1440px] h-[1024px]">
+            <SmartLivestock />
+            <Paragraph />
+            <WithAiAndIot />
           </div>
-        </nav>
-        <div className="flex-1 flex flex-col gap-20 max-w-5xl p-5">
-          <Hero />
-          <main className="flex-1 flex flex-col gap-6 px-4">
-            <h2 className="font-medium text-xl mb-4">Next steps</h2>
-            {hasEnvVars ? <SignUpUserSteps /> : <ConnectSupabaseSteps />}
-          </main>
         </div>
 
-        <footer className="w-full flex items-center justify-center border-t mx-auto text-center text-xs gap-8 py-16">
-          <p>
-            Powered by{" "}
-            <a
-              href="https://supabase.com/?utm_source=create-next-app&utm_medium=template&utm_term=nextjs"
-              target="_blank"
-              className="font-bold hover:underline"
-              rel="noreferrer"
-            >
-              Supabase
-            </a>
-          </p>
-          <ThemeSwitcher />
-        </footer>
+        <div className="relative w-[1440px] h-[1024px] mb-[-1024.00px] bg-secondary">
+          <div className="flex w-[1440px] h-[1024px] items-start justify-between px-[154px] py-[124px] relative">
+            <div className="inline-flex flex-col items-start gap-6 relative flex-[0_0_auto] mb-[-13.00px]">
+              <div className="inline-flex items-center justify-center gap-2.5 px-4 py-3 relative flex-[0_0_auto] bg-white rounded-[7px]">
+                <div className="relative w-fit mt-[-1.00px] font-h3 font-[number:var(--h3-font-weight)] text-black text-[length:var(--h3-font-size)] tracking-[var(--h3-letter-spacing)] leading-[var(--h3-line-height)] whitespace-nowrap [font-style:var(--h3-font-style)]">
+                  Our Product
+                </div>
+              </div>
+
+              <div className="relative w-[569.42px] h-[714px]">
+                <img
+                  className="absolute top-[207px] left-0 w-[381px] h-[507px] aspect-[0.75]"
+                  alt="Element"
+                  src={x21}
+                />
+
+                <img
+                  className="absolute top-0 left-[69px] w-[171px] h-[363px] aspect-[0.47]"
+                  alt="Element"
+                  src={x41}
+                />
+
+                <img
+                  className="absolute top-[251px] left-[399px] w-[170px] h-[367px] aspect-[0.46]"
+                  alt="Element"
+                  src={x51}
+                />
+              </div>
+            </div>
+
+            <Frame />
+          </div>
+        </div>
+
+        <div className="relative w-[1440px] h-[1024px] mb-[-2048.00px] bg-background">
+          <div className="flex w-[1440px] h-[1024px] items-start justify-between px-[154px] py-[124px] relative">
+            <FrameWrapper />
+            <div className="inline-flex flex-col items-end gap-6 relative flex-[0_0_auto]">
+              <div className="inline-flex items-center justify-end gap-2.5 px-4 py-3 relative flex-[0_0_auto] bg-secondary rounded-[7px]">
+                <div className="relative w-fit mt-[-1.00px] font-h3 font-[number:var(--h3-font-weight)] text-black text-[length:var(--h3-font-size)] tracking-[var(--h3-letter-spacing)] leading-[var(--h3-line-height)] whitespace-nowrap [font-style:var(--h3-font-style)]">
+                  Our System
+                </div>
+              </div>
+
+              <div className="relative w-[576px] h-[692px]">
+                <img
+                  className="absolute top-0 left-0 w-[576px] h-[333px] aspect-[1.73] object-cover"
+                  alt="Dashboard pympro"
+                  src={dashboardPympro1}
+                />
+
+                <img
+                  className="absolute top-[357px] left-0 w-[576px] h-[335px] aspect-[1.72] object-cover"
+                  alt="Livestock details"
+                  src={livestockDetailsFs2Dhx1}
+                />
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-    </main>
+
+      <Navbar
+        className="!h-px !absolute !left-[calc(50.00%_-_512px)] !top-[72px]"
+        frameClassName="!mt-[-37.50px] !mb-[-37.50px]"
+        img="line-2-2.svg"
+        line="line-1-2.svg"
+      />
+    </div>
   );
-}
+};
